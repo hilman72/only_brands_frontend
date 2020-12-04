7c6561d1ae0e08c5d80c9855dc682ee3e057240a
Adrian Chan <adrianchanuk@gmail.com>
2020-12-04 17:22:07 +0800

    Added Carousel & Buttons

--- src/Components/RecommendationCarousel/RecommendationCarousel.components.jsx
+++ src/Components/RecommendationCarousel/RecommendationCarousel.components.jsx
@@ -2,13 +2,46 @@ import React from 'react';
 import './RecommendationCarousel.style.scss'
 import RecommendationCard from '../../Components/RecommendationCards/RecommendationCards.components'
 
+//Swiper 
+import { Swiper, SwiperSlide} from 'swiper/react';
+
+//Swiper styling
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/swiper.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// import Swiper core and required components
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+
 function RecommendationCarousel(){
     
     return(
         <div className="RecommendationCarousel">
-            <RecommendationCard />
-            <RecommendationCard />
-            <RecommendationCard />
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={3}
+                navigation
+                scrollbar={{ draggable: true }}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            <SwiperSlide> <RecommendationCard /></SwiperSlide>
+            </Swiper>
         </div>
     )
 }
