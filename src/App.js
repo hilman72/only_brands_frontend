7f34f4064184ad4ce241569f8b5b3c6e48392ca5
hilman72 <hilman91112@gmail.com>
2020-12-07 11:28:41 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import "./App.css";
 import React from "react";
-// import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import UserProfile from "./Pages/UserProfile/UserProfile.components";
 
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <Switch>
-//           <Route path="/UserProfiles" component={UserProfiles} />
-//         </Switch>
-//       </div>
-//     );
-//   }
-// }
-
-const App=()=> {
-  return (
-    <div className="App">
-      <UserProfile />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/UserProfiles" component={UserProfile} />
+        </Switch>
+      </div>
+    );
+  }
 }
+
+// const App=()=> {
+//   return (
+//     <div className="App">
+//       <UserProfile />
+//     </div>
+//   );
+// }
 
 export default App;
