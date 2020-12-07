77a5f7030ad2ad163a09b31953772a480252ae54
Adrian Chan <adrianchanuk@gmail.com>
2020-12-07 14:05:15 +0800

    Added NavBar(not complete)

--- src/Components/Header/Header.component.jsx
+++ src/Components/Header/Header.component.jsx
@@ -1,37 +1,270 @@
-import React from "react";
-import "./Header.style.scss";
-import Grid from "@material-ui/core/Grid";
+import React from 'react';
+import './Header.style.scss'
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Fab from '@material-ui/core/Fab';
+import Button from '@material-ui/core/Button'
 
-import SearchBar from "../../Components/SearchBar/SearchBar";
+// import NotificationsIcon from '@material-ui/icons/Notifications';
+import HomeIcon from '@material-ui/icons/Home';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Logo from '../../Images/logo.png'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
 
-function Header() {
-  return (
-    <div>
-      <div class="headerBorder">
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  submitButton: {
+    marginLeft: theme.spacing(3),
+    borderRadius: theme.shape.borderRadius
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    paddingLeft: `calc(1em + ${theme.spacing(1)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '30ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const [selectedValue, setSelectedValue] = React.useState('Coupons');
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleSubmit = (event) => {
+    console.log(event)
+  }
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <LocalOfferIcon />
+          </Badge>
+        </IconButton>
+        <p>Coupons</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
         >
-          <Grid item spacing={3}>
-            (Logo)
-          </Grid>
-          <Grid item spacing={3}>
-            <SearchBar />
-          </Grid>
-          <Grid item spacing={3}>
-            <div>
-              <button>Home</button>
-              <button>My Coupons</button>
-              <button>My Profile</button>
-              <button>Logout</button>
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton color="inherit">
+          <Badge>
+            <ExitToAppIcon />
+          </Badge>
+        </IconButton>
+        <p>Logout</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <img src={Logo} className="Logo" alt="Logo" />
+         
+            <div className={classes.search}>
+              <InputBase
+                placeholder="Search..."
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
             </div>
-          </Grid>
-        </Grid>
-      </div>
+            <div className={classes.filter}>
+              <FormControlLabel
+                value="Coupons"
+                control={
+                  <Radio color="default"
+                    checked={selectedValue === 'Coupons'}
+                    onChange={handleChange}
+                  />}
+                label="Coupons"
+                labelPlacement="start"
+              />
+              <FormControlLabel
+                value="Brands"
+                control={
+                  <Radio color="default"
+                    checked={selectedValue === 'Brands'}
+                    onChange={handleChange}
+                  />}
+                label="Brands"
+                labelPlacement="start"
+              />
+              <FormControlLabel
+                value="Users"
+                control={
+                  <Radio color="default"
+                    checked={selectedValue === 'Users'}
+                    onChange={handleChange}
+                  />}
+                label="Users"
+                labelPlacement="start"
+              />
+              <Button type="submit" startIcon={<SearchIcon />} className={classes.submitButton}
+                size="medium" variant="contained" color="primary"> Search</Button>
+            </div>
+          
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton edge="end" color="inherit">
+              <Fab size="medium" color="primary" aria-label="add">
+                < HomeIcon />
+              </Fab>
+            </IconButton>
+            <IconButton edge="end" color="inherit">
+              <Fab size="medium" color="primary" aria-label="add">
+                <Badge badgeContent={17} color="secondary">
+                  <LocalOfferIcon />
+                </Badge>
+              </Fab>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <Fab size="medium" color="primary" aria-label="add">
+                <AccountCircle />
+              </Fab>
+            </IconButton>
+            <IconButton edge="end" color="inherit">
+              <Fab size="medium" color="primary" aria-label="add">
+                <ExitToAppIcon />
+              </Fab>
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
     </div>
   );
 }
-
-export default Header;
