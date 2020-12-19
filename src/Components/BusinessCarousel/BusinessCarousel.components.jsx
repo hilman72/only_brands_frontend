aa341d35c48d4736143a8cfe0a4e80034b122f74
hilman72 <hilman91112@gmail.com>
2020-12-19 15:40:38 +0800

    kind of finish comfirm coupon

--- src/Components/BusinessCarousel/BusinessCarousel.components.jsx
+++ src/Components/BusinessCarousel/BusinessCarousel.components.jsx
@@ -1,42 +1,54 @@
-import React from 'react';
-import './BusinessCarousel.style.scss'
-import BrandCards from '../BrandCards/BrandCards.components'
-import { Swiper, SwiperSlide} from 'swiper/react';
+import React from "react";
+import "./BusinessCarousel.style.scss";
+import BrandCards from "../BrandCards/BrandCards.components";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 //Swiper styling
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/swiper.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
+import "swiper/components/navigation/navigation.scss";
+import "swiper/swiper.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 // import Swiper core and required components
-import SwiperCore, { Navigation, Pagination, EffectCoverflow } from 'swiper';
-
+import SwiperCore, { Navigation, Pagination, EffectCoverflow } from "swiper";
 
 SwiperCore.use([Navigation, Pagination, EffectCoverflow]);
 
 function BusinessCarousel() {
-
-    return (
-        <div className="BusinessCarousel">
-            <Swiper
-                spaceBetween={20}
-                slidesPerView={3}
-                loop={true}
-                navigation
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-            <SwiperSlide><BrandCards /></SwiperSlide>
-            <SwiperSlide><BrandCards /></SwiperSlide>
-            <SwiperSlide><BrandCards /></SwiperSlide>
-            <SwiperSlide><BrandCards /></SwiperSlide>
-            <SwiperSlide><BrandCards /></SwiperSlide>
-            <SwiperSlide><BrandCards /></SwiperSlide>
-            <SwiperSlide><BrandCards /></SwiperSlide>
-            </Swiper>
-        </div >
-    )
+  return (
+    <div className="BusinessCarousel">
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={3}
+        loop={true}
+        navigation
+        onSlideChange={() => {}}
+        onSwiper={(swiper) => {}}
+      >
+        <SwiperSlide>
+          <BrandCards />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BrandCards />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BrandCards />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BrandCards />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BrandCards />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BrandCards />
+        </SwiperSlide>
+        <SwiperSlide>
+          <BrandCards />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default BusinessCarousel;
--- src/Components/RecommendationCarousel/RecommendationCarousel.components.jsx
+++ src/Components/RecommendationCarousel/RecommendationCarousel.components.jsx
@@ -1,50 +1,80 @@
-import React from 'react';
-import './RecommendationCarousel.style.scss'
-import RecommendationCard from '../../Components/RecommendationCards/RecommendationCards.components'
+import React from "react";
+import "./RecommendationCarousel.style.scss";
+import RecommendationCard from "../../Components/RecommendationCards/RecommendationCards.components";
 
-//Swiper 
-import { Swiper, SwiperSlide} from 'swiper/react';
+//Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
 
 //Swiper styling
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/swiper.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
+import "swiper/components/navigation/navigation.scss";
+import "swiper/swiper.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 // import Swiper core and required components
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y} from 'swiper';
-
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
-
-function RecommendationCarousel(){
-    
-    return(
-        <div className="RecommendationCarousel">
-            <Swiper
-                spaceBetween={30}
-                slidesPerColumnFill={1}
-                slidesPerColumn={2}
-                loop={true}
-                navigation
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
-            >
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            <SwiperSlide> <RecommendationCard /></SwiperSlide>
-            </Swiper>
-        </div>
-    )
+function RecommendationCarousel() {
+  return (
+    <div className="RecommendationCarousel">
+      <Swiper
+        spaceBetween={30}
+        slidesPerColumnFill={1}
+        slidesPerColumn={2}
+        loop={true}
+        navigation
+        onSlideChange={() => {}}
+        onSwiper={(swiper) => {}}
+      >
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+        <SwiperSlide>
+          {" "}
+          <RecommendationCard />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default RecommendationCarousel;
