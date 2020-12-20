c7f2854b6f0288ef7f534cbf4747c7a64a86700d
dariancdev <69747133+dariancdev@users.noreply.github.com>
2020-12-20 14:35:07 +0800

    login register error notyetverified done

--- src/Pages/Notyet_verifyPage/notyet_verify.component.jsx
+++ src/Pages/Notyet_verifyPage/notyet_verify.component.jsx
@@ -2,8 +2,18 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { logoutNowThunk } from "../../Redux/actions";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+
+import NotYetVerifiedCard from "../../Components/NotYetVerifiedCard/NotYetVerifiedCard";
 
 import "./notyet_verify.styles.scss";
+
+const useStyles = makeStyles(() => ({
+  container:{
+    height: '100vh',
+  }
+}));
 
 const NotYet = (props) => {
   const history = useHistory();
@@ -13,10 +23,27 @@ const NotYet = (props) => {
     history.push("/");
   };
 
+  const styles = useStyles();
+
   return (
     <div>
-      <h1> check your fuckin email asshole</h1>
-      <button onClick={handleClick}>go home</button>
+      {/* <h1> check your fuckin email asshole</h1>
+      <button onClick={handleClick}>go home</button> */}
+
+      <div className={styles.positioning}>
+        <Grid container justify="space-between" alignItems="center" className={styles.container} xs={12}>
+              <Grid item xs={4}>
+
+              </Grid>
+              <Grid item xs={4}>
+                      <NotYetVerifiedCard />
+              </Grid>
+              <Grid item xs={4}>
+
+              </Grid>
+        </Grid>
+      </div>
+
     </div>
   );
 };
--- src/Pages/RegisterPage/RegisterPage.component.jsx
+++ src/Pages/RegisterPage/RegisterPage.component.jsx
@@ -1,14 +1,63 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
 
 import "./RegisterPage.style.scss";
 
+import logo from '../../Assets/Images/smallLogo.png'
+
 import RegisterForm from "../../Components/RegisterForm/RegisterForm.component";
+
+const useStyles = makeStyles(({ palette }) => ({
+  logo: {
+    border:"3px solid #ff4c6d",
+    borderRadius:'50%',
+    height: '3rem',
+    padding: '0.5rem',
+    position: 'absolute !important',
+    top: '10px',
+    left: '10px',
+    boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px;',
+  },
+  // button:{
+  //   position: 'absolute !important',
+  //   top: '10px',
+  //   left: '10px',
+  //   border: 'none',
+  //   backgroundColor: 'rgba(0,0,0,0)',
+  //   borderRadius:'50%',
+  // },
+  RegisterBody:{
+    backgroundColor:"#f0f0f0",
+  },
+  toReg:{
+    fontSize:"0.7rem !important",
+    paddingTop:"2rem",
+    color: '#282828',
+    "&:hover":{
+      textDecoration: 'underline',
+      cursor: 'pointer',
+    },
+  }
+}));
 
 const RegisterPage = () => {
 
   const [div, setDiv] = React.useState("registerContainer")
   const [id, setId] = React.useState("User")
   const [id2, setId2] = React.useState("User Name")
+
+  const styles = useStyles();
+
+  const history = useHistory();
+
+  function navigateToHome(){
+    history.push("/");
+  }
+
+  function navigateToLogin(){
+    history.push("/LoginPage")
+  }
 
   function handleChange(){
     setDiv("registerContainer registerRight-panel-active")
@@ -32,6 +81,8 @@ const RegisterPage = () => {
 
   return (
     <div class="registerBody">
+
+      <img src={logo} onClick={navigateToHome} className={styles.logo}/>
 
 
       <div class={div} id="registerContainer">
@@ -61,11 +112,13 @@ const RegisterPage = () => {
             <h2 class="registerH2">Looking for discounts in your favourite restaurant?</h2>
             <p class="registerP">Join OnlyBrands to get sweet offets!</p>
             <button onClick={handleSwitch} class="ghost registerButton" id="signIn">Register</button>
+            <p onClick={navigateToLogin} className={styles.toReg}> Have an account already? Click me to login!</p>
           </div>
           <div class="registerOverlay-panel registerOverlay-right">
             <h2 class="registerH2">Brands looking for more customers?</h2>
             <p class="registerP">Create your fanbase on OnlyBrands!</p>
             <button onClick={handleChange} class="ghost registerButton" id="signUp">Register a Brands Account</button>
+            <p onClick={navigateToLogin} className={styles.toReg}> Have an account already? Click me to login!</p>
           </div>
         </div>
       </div>
