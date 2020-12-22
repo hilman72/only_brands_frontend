3be08e74ece45bf09a6353756730aee0384c844a
hilman72 <hilman91112@gmail.com>
2020-12-22 21:51:09 +0800

    fix

--- src/Pages/TestingPage/TestingPage.jsx
+++ src/Pages/TestingPage/TestingPage.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 
-import "./TestingPage.style.scss";
-import BrandsCardLong from '../../Components/BrandCardsLong/BrandCardsLong.components'
-
 const TestingPage = () => {
   return (
     <div>
-    <BrandsCardLong />
-        <h1>
+      <h1>
         H1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456
-        </h1>
-        <h2>
+      </h1>
+      <h2>
         H2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456
-        </h2>
-        <h3>
+      </h2>
+      <h3>
         H3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456
-        </h3>
-        <h4>
+      </h3>
+      <h4>
         H4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456
-        </h4>
-        <h5>
+      </h4>
+      <h5>
         H5. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456
-        </h5>
-        <h6>
+      </h5>
+      <h6>
         H6. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456
-        </h6>
-        <p>p. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456</p>
-    </div>  );
+      </h6>
+      <p>p. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 123456</p>
+    </div>
+  );
 };
 
 export default TestingPage;
